63d8aa164d914a0970d23db17125bd504958592d
MohammadSulaiman1 <mahdisul1@gmail.com>
2026-05-15 21:13:51 -0400

    gtags

--- midwesttintanddetail/app/layout.tsx
+++ midwesttintanddetail/app/layout.tsx
@@ -66,6 +66,21 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18057181965"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-18057181965');
+            `,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
